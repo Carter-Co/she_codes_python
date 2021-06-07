85d93059ed84ec31064c5d7e3a74af6059a3984f
Carter-Co <84072703+Carter-Co@users.noreply.github.com>
2021-06-07 20:31:42 +0800

    Create user_input_playground.py

---
+++ user_input_playground.py
@@ -0,0 +1,14 @@
+name = input("What is your name?")
+owner = input(Do you have a dog")
+
+print(f"hi {name}!")
+
+age = input (f"Hi {name}, how old are you? ")
+years_until_100 = 100 - int(age)
+years_in_human_years = 7 * int(age)
+
+print(f"Wow {name}! You'll be 100 in {years_until_100} years! ")
+print(f"Hey {name}, {owner}? ") 
+
+
+
