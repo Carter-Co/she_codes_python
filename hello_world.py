298739b6a8ec730e2ca2427a6d4785b6580c14ea
Carter-Co <84072703+Carter-Co@users.noreply.github.com>
2021-06-07 19:52:30 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,33 @@
+print("hello, world!")
+day = "Monday"
+date = "7th"
+month = "June"
+year = "2021"
+print(day)
+print(f"Today is: {day}")
+# This is a comment
+# Create a variable for the current month 
+# output this in the same sentence
+print(f"Today is: {day} the {date} of {month} {year}")
+
+run1_dist = 1400
+print(run1_dist)
+print(type(run1_dist))
+
+run2_dist = 1800
+total_distance = run1_dist + run2_dist 
+difference = run2_dist - run1_dist
+print(total_distance)
+print(difference)
+
+run3_dist = 1.7
+run4_dist = 1.35
+print (type(run4_dist))
+
+print(run1_dist / 1000)
+print(run4_dist * 1000)
+
+run5_dist = "5000"
+print (run5_dist + "3")
+
+
